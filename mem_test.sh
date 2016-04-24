080b6b69a3829c517aa35be4caef3598e349eb9b
neilnelson <nnelson@infowest.com>
2016-04-23 22:06:33 -0600

    mem_test bash file

---
+++ mem_test.sh
@@ -0,0 +1,93 @@
+free
+date +%s
+build/tools/caffe train --solver=test/lenet_solver.prototxt >> test/run.log 2>&1
+rm test/run.log
+date +%s
+build/tools/caffe train --solver=test/lenet_solver.prototxt >> test/run.log 2>&1
+rm test/run.log
+date +%s
+build/tools/caffe train --solver=test/lenet_solver.prototxt >> test/run.log 2>&1
+rm test/run.log
+date +%s
+build/tools/caffe train --solver=test/lenet_solver.prototxt >> test/run.log 2>&1
+rm test/run.log
+date +%s
+build/tools/caffe train --solver=test/lenet_solver.prototxt >> test/run.log 2>&1
+rm test/run.log
+date +%s
+build/tools/caffe train --solver=test/lenet_solver.prototxt >> test/run.log 2>&1
+rm test/run.log
+date +%s
+build/tools/caffe train --solver=test/lenet_solver.prototxt >> test/run.log 2>&1
+rm test/run.log
+date +%s
+build/tools/caffe train --solver=test/lenet_solver.prototxt >> test/run.log 2>&1
+rm test/run.log
+date +%s
+build/tools/caffe train --solver=test/lenet_solver.prototxt >> test/run.log 2>&1
+rm test/run.log
+date +%s
+build/tools/caffe train --solver=test/lenet_solver.prototxt >> test/run.log 2>&1
+rm test/run.log
+date +%s
+build/tools/caffe train --solver=test/lenet_solver.prototxt >> test/run.log 2>&1
+rm test/run.log
+date +%s
+build/tools/caffe train --solver=test/lenet_solver.prototxt >> test/run.log 2>&1
+rm test/run.log
+date +%s
+build/tools/caffe train --solver=test/lenet_solver.prototxt >> test/run.log 2>&1
+rm test/run.log
+date +%s
+build/tools/caffe train --solver=test/lenet_solver.prototxt >> test/run.log 2>&1
+rm test/run.log
+date +%s
+build/tools/caffe train --solver=test/lenet_solver.prototxt >> test/run.log 2>&1
+rm test/run.log
+date +%s
+build/tools/caffe train --solver=test/lenet_solver.prototxt >> test/run.log 2>&1
+rm test/run.log
+date +%s
+build/tools/caffe train --solver=test/lenet_solver.prototxt >> test/run.log 2>&1
+rm test/run.log
+date +%s
+build/tools/caffe train --solver=test/lenet_solver.prototxt >> test/run.log 2>&1
+rm test/run.log
+date +%s
+build/tools/caffe train --solver=test/lenet_solver.prototxt >> test/run.log 2>&1
+rm test/run.log
+date +%s
+build/tools/caffe train --solver=test/lenet_solver.prototxt >> test/run.log 2>&1
+rm test/run.log
+date +%s
+build/tools/caffe train --solver=test/lenet_solver.prototxt >> test/run.log 2>&1
+rm test/run.log
+date +%s
+build/tools/caffe train --solver=test/lenet_solver.prototxt >> test/run.log 2>&1
+rm test/run.log
+date +%s
+build/tools/caffe train --solver=test/lenet_solver.prototxt >> test/run.log 2>&1
+rm test/run.log
+date +%s
+build/tools/caffe train --solver=test/lenet_solver.prototxt >> test/run.log 2>&1
+rm test/run.log
+date +%s
+build/tools/caffe train --solver=test/lenet_solver.prototxt >> test/run.log 2>&1
+rm test/run.log
+date +%s
+build/tools/caffe train --solver=test/lenet_solver.prototxt >> test/run.log 2>&1
+rm test/run.log
+date +%s
+build/tools/caffe train --solver=test/lenet_solver.prototxt >> test/run.log 2>&1
+rm test/run.log
+date +%s
+build/tools/caffe train --solver=test/lenet_solver.prototxt >> test/run.log 2>&1
+rm test/run.log
+date +%s
+build/tools/caffe train --solver=test/lenet_solver.prototxt >> test/run.log 2>&1
+rm test/run.log
+date +%s
+build/tools/caffe train --solver=test/lenet_solver.prototxt >> test/run.log 2>&1
+# rm test/run.log
+date +%s
+free
